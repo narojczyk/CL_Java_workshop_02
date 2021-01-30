e44a53f527532e57199cd218fb427bfd30d845bf
Jakub W. Narojczyk <narojczyk@ifmpan.poznan.pl>
2021-01-30 13:01:10 +0100

    initial

---
+++ src/main/java/workshop02.java
@@ -0,0 +1,11 @@
+import java.sql.Connection;
+
+public class workshop02 {
+    public static void main(String[] args) {
+
+
+//        try(Connection conn  = MyDBTools.mySQLConnect("workshop2")){
+////             DBUtil.insert(conn, "INSERT INTO products(name,description,price) VALUES(?,?,?)", "Krzesło", "Bujane", "100.00");
+//        }
+    }
+}
